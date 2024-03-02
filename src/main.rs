ae47444fe0efd0a23ec4df8ee4e6a115db1e1470
joaoleal <devjoaoleal1006@gmail.com>
2024-03-02 20:34:58 -0300

    add: render function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 mod watcher;
-use watcher::loader::Branch;
+use watcher::content_parser::Branch;
+use watcher::content_renderer::Renderer;
 use std::path::PathBuf;
 use std::env::current_dir;
 
 fn main() { 
-    let path_to_content =  PathBuf::from(current_dir().unwrap()).join(PathBuf::from("content/")); 
-    let mut content_tree =  Branch::load_content(path_to_content);
+    let path_to_content =  PathBuf::from(current_dir().unwrap()).join(PathBuf::from("test_content/")); 
+    let mut content_tree =  Branch::load_content(path_to_content).unwrap();
+    let mut render = Renderer::new(&content_tree, PathBuf::from(current_dir().unwrap()).join(PathBuf::from("output/")));
+    match render.render(None) {
+        Ok(_) => {
+            println!("Rendered content successfully");
+        }
+        Err(err) => {
+            println!("Error rendering content: {:?}", err);
+        }
+    }
 }
--- src/watcher.rs
+++ src/watcher.rs
@@ -1,18 +1,21 @@
-pub mod loader {
+pub mod content_parser {
+    use std::any::type_name;
     use std::path::PathBuf;
+    
     use anyhow::Result;
 
     #[derive(Debug)]
     pub struct Branch {
-        path: PathBuf, 
-        taipe: ContentCategories,
-        children: Option<Vec<Branch>>,
+        pub path: PathBuf, 
+        pub taipe: ContentCategories,
+        pub children: Option<Vec<Branch>>,
     }
     #[derive(Debug)]
     pub enum ContentCategories {
         Section(String),
         Post(String),
     }
+
     impl Branch{
         pub fn load_content(path: PathBuf)-> Result<Vec<Branch>> {
             let mut ret =  Vec::<Branch>::new();
@@ -43,5 +46,56 @@ pub mod loader {
             }
         }
     }
-
+}
+pub mod content_renderer {
+    use crate::watcher::content_parser::Branch;
+    use std::{any::{type_name, Any}, fs, io::{Read, Write}, path::PathBuf};
+    use anyhow::{Ok, Result};
+    use markdown::to_html;
+    use yew::html;
+    pub struct Renderer<'a> {
+        pub content_tree: &'a Vec<Branch>,
+        pub output_path: PathBuf,
+    }
+    impl Renderer<'_>{
+        pub fn new(content_tree: &Vec<Branch>, output_path: PathBuf) -> Renderer<'_> {    
+            Renderer {
+                content_tree,
+                output_path,
+            }
+        }
+        
+        pub fn render(&self, section: Option<String>) -> Result<()> {
+            for branch in self.content_tree {
+                match &branch.children {
+                    Some(children) => {
+                        let mut path = self.output_path.clone();
+                        path.push(&branch.path);
+                        std::fs::create_dir_all(&path)?;
+                        let mut renderer = Renderer::new(children, path.clone());
+                        renderer.render( Some(path.to_str().unwrap().to_string()))?;
+                    }
+                    None => {
+                        let mut md_content = String::new();
+                        let mut html_content = String::new();
+                        
+                        if branch.path.file_name().unwrap().to_str().unwrap().ends_with(".md") {
+                            match std::fs::File::open(&branch.path) {
+                                Result::Ok(mut file) => {
+                                    file.read_to_string(&mut md_content)?;
+                                    html_content = to_html(&md_content);
+                                }
+                                Err(err) => {
+                                    return Err(anyhow::Error::new(err));
+                                }
+                                
+                            }
+                        }
+                        println!("{:?}", html_content);
+                    }
+                };
+            };
+            Ok(())
+        }
+    }
 }
